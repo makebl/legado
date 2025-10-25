3992493743cd8368246809c98c3ed6198a4d75e2
马可 <78850005+makebl@users.noreply.github.com>
2025-10-25 23:51:18 +0800

    Add files via upload

--- app/src/main/java/io/legado/app/help/update/AppUpdateGitHub.kt
+++ app/src/main/java/io/legado/app/help/update/AppUpdateGitHub.kt
@@ -26,9 +26,9 @@ object AppUpdateGitHub : AppUpdate.AppUpdateInterface {
 
     private suspend fun getLatestRelease(): List<AppReleaseInfo> {
         val lastReleaseUrl = if (checkVariant.isBeta()) {
-            "https://api.github.com/repos/gedoor/legado/releases/tags/beta"
+            "https://api.github.com/repos/makebl/legado/releases/tags/beta"
         } else {
-            "https://api.github.com/repos/gedoor/legado/releases/latest"
+            "https://api.github.com/repos/makebl/legado/releases/latest"
         }
         val res = okHttpClient.newCallResponse {
             url(lastReleaseUrl)
@@ -67,3 +67,4 @@ object AppUpdateGitHub : AppUpdate.AppUpdateInterface {
         }.timeout(10000)
     }
 }
+
